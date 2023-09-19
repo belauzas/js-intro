bb9d356ed9d700fc429ada07fe6a8ee57a9afa31
Rimantas <belauzas@gmail.com>
2023-09-19 10:46:28 +0300

    JS comments

---
+++ index.js
@@ -0,0 +1,23 @@
+// 1. Kintamieji - kur laikoma informacija
+// 2. Palyginimas / salygos - "jei ...., priesingu atveju ..."
+// 3. Ciklai - kartojimai...
+// 4. Funkcijos - loginiu zingsniu visuma, kuria galima atkartoti, kai reikia
+// 5. Ivykiai - kai vartotojas: paspaudzia mygtuka...
+
+
+/*
+    1. Kintamieji - kur laikoma informacija
+    2. Palyginimas / salygos - "jei ...., priesingu atveju ..."
+    3. Ciklai - kartojimai...
+    4. Funkcijos - loginiu zingsniu visuma, kuria galima atkartoti, kai reikia
+    5. Ivykiai - kai vartotojas: paspaudzia mygtuka...
+*/
+
+
+// Single line comment
+
+/* 
+    Multi
+    line
+    comment
+*/
