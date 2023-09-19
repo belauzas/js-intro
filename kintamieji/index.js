39e9b494d0cdf03c79d1c1342604773ac976e7fe
Rimantas <belauzas@gmail.com>
2023-09-19 11:15:20 +0300

    Numbers

---
+++ kintamieji/index.js
@@ -0,0 +1,9 @@
+/*
+KINTAMIEJI
+
+- number
+- string (tekstas)
+- boolean (true / false)
+- array (masyvas / kolekcija / sąrašas / listas / matrica / arėjas)
+- object (objektas - kompleksinei informacijai aprasyti)
+*/
