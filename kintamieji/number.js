313b6488d9d32f94283952fbcb2ff51e16f25887
Rimantas <belauzas@gmail.com>
2023-09-20 10:30:53 +0300

    Artimetika ir nodemon install

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -11,7 +11,6 @@ Iniciavimo budas:
 - var (niekada 👀)
 */
 
-
 const amzius = 99;
 let pinigine = 0;
 var minusas = -7.56;
@@ -20,9 +19,43 @@ console.log(amzius);
 console.log(pinigine);
 console.log(minusas);
 
-
 const x1 = 1;
 const x2 = 2;
 const x3 = 3;
 const x4 = 4;
 const x5 = 5;
+
+const n1 = 7;
+const n2 = 5;
+
+const sum = n1 + n2;
+console.log(sum);
+
+const diff = n1 - n2;
+console.log(diff);
+
+const multi = n1 * n2;
+console.log(multi);
+
+const div = n1 / n2;
+console.log(div);
+
+const complex = (2 + 2) * 2;
+console.log(complex);
+
+const exp1 = 2 + 2;
+const exp2 = 2 * 2;
+const exp3 = 2 ** 2;
+
+console.log(exp1, exp2, exp3);
+
+const exp4 = 2 ** 0;
+const exp5 = 2 ** 1;
+const exp6 = 2 ** 2;
+const exp7 = 2 ** 3;
+const exp8 = 2 ** 4;
+
+console.log(exp4, exp5, exp6, exp7, exp8);
+
+const exp9 = 3 ** 3;
+console.log(exp9);
