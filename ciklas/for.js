53508579cf7dd8f9ce0dbe105badb2031daa0b66
Rimantas <belauzas@gmail.com>
2023-09-28 11:28:12 +0300

    For

--- ciklas/for.js
+++ ciklas/for.js
@@ -32,8 +32,8 @@ console.clear();
 
 // Skaiciai intervale nuo 3 iki 8 imtinai yra: 3 4 5 6 7 8.
 
-const start = -8;
-const finish = -5;
+const start = 3;
+const finish = 8;
 let numberSequence = '';
 
 for (let i = start; i <= finish; i++) {
@@ -41,4 +41,58 @@ for (let i = start; i <= finish; i++) {
 }
 
 const numberLine = `Skaiciai intervale nuo ${start} iki ${finish} imtinai yra:${numberSequence}.`;
+
 console.log(numberLine);
+
+
+// Skaiciai intervale nuo 3 iki 8 imtinai yra: 3, 4, 5, 6, 7, 8.
+
+// const numberSequence2 = '3, 4, 5, 6, 7, 8';
+let numberSequence2 = start;
+
+for (let i = start + 1; i <= finish; i++) {
+    numberSequence2 += ', ' + i;
+}
+
+const numberLine2 = `Skaiciai intervale nuo ${start} iki ${finish} imtinai yra: ${numberSequence2}.`;
+
+console.log(numberLine2);
+
+console.clear();
+
+function arTestiCikla(index) {
+    if (index <= 10) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+for (let i = 0; arTestiCikla(i); i += 2) {
+    console.log(i, 'ciklas');
+}
+
+console.clear();
+
+const word = 'zodelis';
+
+for (let i = 0; i < word.length; i++) {
+    console.log(i, `Raide: ${word[i]}`);
+}
+
+console.clear();
+
+//              0  1  2  3  4
+const marks = [10, 2, 8, 4, 6];
+
+let totalSum = 0;
+const amount = marks.length;
+
+for (let i = 0; i < amount; i++) {
+    const mark = marks[i];
+    totalSum += mark;
+    console.log(`${i})`, mark, '=', totalSum);
+}
+
+const average = totalSum / amount;
+console.log(marks, 'average is', average);
